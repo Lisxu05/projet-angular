a629b650d920d8fcd5cf7ce7fbb981aa79c30a45
Lisa <lisa.roselier@gmail.com>
2025-10-14 22:50:38 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,38 @@
-// import { Component, OnInit } from '@angular/core';
-// import { AirtableService } from './services/airtable.service';
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html'
-// })
-// export class AppComponent implements OnInit {
-//   airtableData: any;
-
-//   constructor(private airtableService: AirtableService) {}
-
-//   ngOnInit(): void {
-//     this.airtableService.getData().subscribe({
-//       next: (data) => this.airtableData = data,
-//       error: (err) => console.error('Erreur API:', err)
-//     });
-//   }
-// }
-
-
-import { Component } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { Component, OnInit } from '@angular/core';
+import { AirtableService } from './services/airtable.service';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  template: `<h1>Bienvenue sur mon portfolio Angular !</h1>`,
+  templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  private airtableData: any;
+
+  public constructor(
+    private airtableService: AirtableService
+  ) {}
+
+  public ngOnInit(): void {
+    this.airtableService.getData()
+    .pipe(
+      tap((data) => {
+        console.log('Données récupérées:', data);
+        this.airtableData = data;
+      })
+    )
+    .subscribe();
+  }
+}
+
+
+// import { Component } from '@angular/core';
+// import { bootstrapApplication } from '@angular/platform-browser';
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   template: `<h1>Bienvenue sur mon portfolio Angular !</h1>`,
+// })
+// export class AppComponent {}
