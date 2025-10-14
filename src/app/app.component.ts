32028503e03b1c7d97490267de98156051a201e5
Lisa <lisa.roselier@gmail.com>
2025-10-15 00:01:15 +0200

    cest good

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,37 +2,41 @@ import { Component, OnInit } from '@angular/core';
 import { AirtableService } from './services/airtable.service';
 import { tap } from 'rxjs';
 
+type AirtableRecord = {
+  nom: string,
+  email: string,
+  message: string,
+  entreprise: string,
+  prenom: string
+};
+
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html'
 })
+
 export class AppComponent implements OnInit {
 
-  public airtableData: any;
+  public airtableData: AirtableRecord[] = [];
 
   public constructor(
     private airtableService: AirtableService
-  ) {}
+  ) { }
 
   public ngOnInit(): void {
     this.airtableService.getData()
-    .pipe(
-      tap((data) => {
-        console.log('Données récupérées:', data);
-        this.airtableData = data;
-      })
-    )
-    .subscribe();
+      .pipe(
+        tap((data) => {
+          console.log('Données récupérées:', data.data);
+          this.airtableData = data.data;
+          console.log(this.airtableData);
+          console.log(this.airtableData.at(0)?.nom);
+        })
+      )
+      .subscribe();
+
+      
   }
 }
 
-
-// import { Component } from '@angular/core';
-// import { bootstrapApplication } from '@angular/platform-browser';
-
-// @Component({
-//   selector: 'app-root',
-//   standalone: true,
-//   template: `<h1>Bienvenue sur mon portfolio Angular !</h1>`,
-// })
-// export class AppComponent {}
